0d831bd74e99fd29b6251ea9a6c8ded8409cd4d9
apexadmin1818 <admin@apexnyc.com>
2025-06-05 22:03:14 -0400

    Document meditation calendar

--- app/src/main/java/org/yuttadhammo/BodhiTimer/Animation/TimerAnimation.kt
+++ app/src/main/java/org/yuttadhammo/BodhiTimer/Animation/TimerAnimation.kt
@@ -1,18 +1,18 @@
 /*
-    This file is part of Bodhi Timer.
+    This file is part of Divine Timer.
 
-    Bodhi Timer is free software: you can redistribute it and/or modify
+    Divine Timer is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Bodhi Timer is distributed in the hope that it will be useful,
+    Divine Timer is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Bodhi Timer.  If not, see <http://www.gnu.org/licenses/>.
+    along with Divine Timer.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.yuttadhammo.BodhiTimer.Animation
 
